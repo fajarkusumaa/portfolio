f958fa5a765281b04b2d2c8b2910911f857bf4df
fajarkusumaa <fajarkusuma46@gmail.com>
2023-06-09 14:46:59 +0700

    update profile

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+// import React from "react";
 import "./Hero.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Hero = () => {
     return (
@@ -10,10 +10,15 @@ const Hero = () => {
                 <div className="flex flex-col items-end justify-start gap-4">
                     <div className="flip-card my-btn p-0 w-36 h-20 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-h">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl bg-center"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/1.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back rounded-xl bg-zinc-900">
                                 <a href="https://github.com/fajarkusumaa">
                                     <i className="fa-brands fa-github fa-2xl text-white"></i>
                                 </a>
@@ -23,12 +28,17 @@ const Hero = () => {
 
                     <div className="flip-card my-btn p-0 w-48 h-36 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-h">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/2.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back rounded-xl bg-zinc-900">
                                 <a href="">
-                                    <i className="fa-brands fa-github fa-2xl text-white"></i>
+                                    <h2 className="text-white">My Works</h2>
                                 </a>
                             </div>
                         </div>
@@ -36,11 +46,16 @@ const Hero = () => {
 
                     <div className="flip-card my-btn p-0 w-36 h-36 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-h">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back rounded-xl bg-gray-600">
-                                <a href="">
+                            <div
+                                className="flip-card-front rounded-xl"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/3.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back rounded-xl bg-zinc-900">
+                                <a href="https://dribbble.com/fajarksm">
                                     <i className="fa-brands fa-dribbble fa-2xl text-white"></i>
                                 </a>
                             </div>
@@ -53,36 +68,55 @@ const Hero = () => {
                 <div className="flex flex-col items-end justify-start gap-4 ">
                     <div className="flip-card my-btn p-0 w-64 h-36 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-v">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back-v rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/4.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back-v rounded-xl bg-zinc-900">
                                 <a href="">
                                     <i className="fa-brands fa-github fa-2xl text-white"></i>
                                 </a>
                             </div>
                         </div>
                     </div>
+
                     <div className="flip-card my-btn p-0 w-64 h-64 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-v">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back-v rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/5.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back-v rounded-xl bg-zinc-900">
                                 <a href="">
                                     <i className="fa-brands fa-github fa-2xl text-white"></i>
                                 </a>
                             </div>
                         </div>
                     </div>
+
                     <div className="flip-card my-btn p-0 w-64 h-48 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-v">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back-v rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/6.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back-v rounded-xl bg-zinc-900">
                                 <a href="">
-                                    <i className="fa-brands fa-github fa-2xl text-white"></i>
+                                    <p className="text-2xl text-white">
+                                        Let's Talk
+                                    </p>
                                 </a>
                             </div>
                         </div>
@@ -96,12 +130,14 @@ const Hero = () => {
                         <div className="flip-card-inner-h border-zinc-900">
                             <div
                                 className="flip-card-front rounded-xl bg-cover bg-center"
-                                // style={{
-                                //     backgroundImage: url(./)
-                                // }}
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/7.png"
+                                    )`
+                                }}
                             ></div>
-                            <div className="flip-card-back rounded-xl bg-gray-600">
-                                <a href="">
+                            <div className="flip-card-back rounded-xl bg-zinc-900">
+                                <a href="https://instagram.com/fajarksm?igshid=ZDc4ODBmNjlmNQ==">
                                     <i className="fa-brands fa-instagram fa-2xl text-white"></i>
                                 </a>
                             </div>
@@ -110,10 +146,15 @@ const Hero = () => {
 
                     <div className="flip-card my-btn w-48 h-24 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-h border-zinc-900">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/8.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back rounded-xl bg-zinc-900">
                                 <a href="">
                                     <i className="fa-brands fa-instagram fa-2xl text-white"></i>
                                 </a>
@@ -123,10 +164,15 @@ const Hero = () => {
 
                     <div className="flip-card my-btn w-24 h-24 rounded-xl overflow-hidden">
                         <div className="flip-card-inner-h border-zinc-900">
-                            <div className="flip-card-front rounded-xl">
-                                <img src="" alt="" />
-                            </div>
-                            <div className="flip-card-back rounded-xl bg-gray-600">
+                            <div
+                                className="flip-card-front rounded-xl bg-slate-700"
+                                style={{
+                                    backgroundImage: `url(
+                                        "../src/assets/bg-profile/9.png"
+                                    )`
+                                }}
+                            ></div>
+                            <div className="flip-card-back rounded-xl bg-zinc-900">
                                 <a href="">
                                     <i className="fa-brands fa-instagram fa-2xl text-white"></i>
                                 </a>
